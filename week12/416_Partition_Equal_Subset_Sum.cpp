f421c793e79e67ad4bbb0c51773de06093c35c67
qqqqq4545 <socool901107@gmail.com>
2025-05-19 00:08:21 +0800

    upload 416_Partition_Equal_Subset_Sum

---
+++ week12/416_Partition_Equal_Subset_Sum.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    bool canPartition(vector<int>& nums) {
+        int sum = 0;
+        for (int num : nums) {
+            sum += num;
+        }
+        
+        if (sum % 2 != 0) {
+            return false;
+        }
+        
+        int target = sum / 2;
+        int n = nums.size();
+        
+        vector<bool> dp(target + 1, false);
+        dp[0] = true;
+        
+        for (int num : nums) {
+            for (int i = target; i >= num; i--) {
+                dp[i] = dp[i] || dp[i - num];
+            }
+        }
+        
+        return dp[target];
+    }
+};
